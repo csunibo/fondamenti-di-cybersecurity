09af763d42643e64dc6d340e091a1f79da2d0010
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-03-06 21:41:52 +0100

    add bandit solutions

---
+++ prove/esercitazioni/parziale1-2023-03-03-soluzione.sh
@@ -0,0 +1,33 @@
+# Level 0 -> Level 1
+cat readme
+
+# Level 1 -> Level 2
+cat ./-
+
+# Level 2 -> Level 3
+cat "spaces in this filename"
+
+# Level 3 -> Level 4
+ls -a inhere/
+cat inhere/.hidden
+
+# Level 4 -> Level 5
+file ./*
+cat ./-file07
+
+# Level 5 -> Level 6
+xargs file $(find -size 1033c ! -executable)
+cat ./inhere/maybeinhere07/.file2
+
+# Level 6 -> Level 7
+find / -user bandit7 -group bandit6 -size 33c
+cat /var/lib/dpkg/info/bandit7.password
+
+# Level 7 -> Level 8
+cat data.txt | grep millionth
+
+# Level 9 -> Level 9
+sort data.txt | uniq -c | sort -r | head -n 1 -
+
+# Level 9 -> Level 10
+cat data.txt | strings | grep ===
